7cc3ece4dc55802954136592ff907cb29c36ded9
Jonatan Serna <jonatansernahiguera@gmail.com>
2024-01-05 15:32:12 -0500

    deploy

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,11 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCRJ5__y_HhtFM2IM3pLY9nA&part=snippet%2Cid&order=date&maxResults=9';
 
 const content = null || document.getElementById('content');
 
 const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '6540473ff4mshfbdbb244ab22f99p10c708jsnfd294791e746',
+        'X-RapidAPI-Key': '8415f06a8amsh3315f101fec9136p18826ajsnf257e16f8655',
 		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
 };
@@ -17,35 +17,29 @@ async function fetchData(urlApi) {
 }
 
 (async () => {
-    //Dentro implementamos la lógica necesaria para hacer el llamado a la API, obtener los elementos y mostrarlos en html
-    //Se implementa try y catch
-    try{
-    } catch {
-    }
-})();
-
-(async () => {
     try {
         const videos = await fetchData(API);
         let view = `
         ${videos.items.map(video => `
+        <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
             <div class="group relative">
-                <div
-                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                    <div
+                        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                        <h3 class="text-sm text-black-100" style="color:black;">
+                            <span aria-hidden="true" class="absolute inset-0"></span>
+                            ${video.snippet.title}
+                        </h4>
+                    </div>
                 </div>
-                <div class="mt-4 flex justify-between">
-                    <h3 class="text-sm text-gray-700" style="color:white;">
-                        <span aria-hidden="true" class="absolute inset-0"></span>
-                        ${video.snippet.title}
-                    </h4>
-                </div>
-            </div>
+            </a>
         `).slice(0, 4).join('')}
         `;
         content.innerHTML = view;
-    } catch {
-     
+    } catch (error) {
+        console.log(error);
     }
 })();
 
